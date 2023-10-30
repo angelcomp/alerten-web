102cdf4fabf7c492ae5e5bd672fe1b806ff55e56
Angelica dos Santos <santosangelicassp@gmail.com>
2023-10-30 12:12:45 -0300

    presentation screen
    static welcome page

---
+++ scenes/presentation/script.js
@@ -0,0 +1,19 @@
+function olar() {
+    alert("olarrrrrrrrrrr")
+    console.log("olar2")
+}
+
+function showDetails(card) {
+    card.innerHTML = `<div class="item-card" style="
+        background-color: rgb(83, 83, 83); 
+        color: white; 
+        text-align: center;
+        font-weight: 400; 
+        padding: 8px; 
+        height: 200px;
+        width: 150px;
+        border-radius: 16px;
+        box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum veniam, cumque ab quia culpa illum esse</p>
+    </div>`
+}
