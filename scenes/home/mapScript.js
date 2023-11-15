870992e8ed11b2ee37e87450cd958e1757fe3906
Angelica dos Santos <santosangelicassp@gmail.com>
2023-11-14 23:36:18 -0300

    some fixes

--- scenes/home/mapScript.js
+++ scenes/home/mapScript.js
@@ -1,7 +1,7 @@
 
 var allMarkers = {}
+var lastUncreatedMarkerId = -1
 
-// Don't forget to replace <YOUR_ACCESS_TOKEN> by your real access token!
 const map = new maplibregl.Map({
     container: "map",
     style: `https://api.maptiler.com/maps/streets-v2/style.json?key=PRZRDk869lSZIQ2Szsuo`,
@@ -21,19 +21,18 @@ scale.setUnit('kilometers');
 
 maplibregl.setRTLTextPlugin("https://unpkg.com/@mapbox/mapbox-gl-rtl-text@0.2.3/mapbox-gl-rtl-text.min.js");
 
-var isMapContentClosed = true
-var arrowButton = document.getElementById("arrow-open-map")
-function openMapcontent() {
-    if (isMapContentClosed) {
+var isFormClosed = true
+
+function openForm() {
+    if (isFormClosed) {
         document.getElementById("map-dashboard").style.gridTemplateColumns = "2fr 3fr";
         document.getElementById("map-content-form").style.display = "grid";
-        arrowButton.style.transform = "rotate(180deg)"
-        isMapContentClosed = false
+        isFormClosed = false
     } else {
         document.getElementById("map-dashboard").style.gridTemplateColumns = "1fr";
         document.getElementById("map-content-form").style.display = "none";
-        arrowButton.style.transform = "rotate(0deg)"
-        isMapContentClosed = true
+        document.getElementById("add-on-map").style.display = "block"
+        isFormClosed = true
     }
 }
 
@@ -41,21 +40,22 @@ var isAddMarkerActive = false
 map.on('click', this.addMarker.bind(this));
 
 function activeMap() {
+    document.getElementById("add-on-map").style.display = "none"
 
     if(isAddMarkerActive) {
         document.getElementById("info-add-marker").style.display = "none";
         map.setLayoutProperty('escola', 'visibility', 'visible');
-        document.getElementById("add-on-map").style.transform = "rotate(0deg)"
         isAddMarkerActive = false
     } else {
         document.getElementById("info-add-marker").style.display = "block";
         map.setLayoutProperty('escola', 'visibility', 'none');
-        document.getElementById("add-on-map").style.transform = "rotate(45deg)"
         isAddMarkerActive = true
     }
 }
 
 function addMarker(event) {
+    lastUncreatedMarkerId = Date.now()
+
     if(isAddMarkerActive) {
         var coordinates = event.lngLat;
         
@@ -66,10 +66,10 @@ function addMarker(event) {
         
         var marker = new maplibregl.Marker().setLngLat(coordinates).setPopup(markerPopup).addTo(map);
         
-        allMarkers[Date.now()] = marker
+        allMarkers[lastUncreatedMarkerId] = marker
         
-        activeMap()
-        openMapcontent()
+        activeMap() // in this case it will disable add marker on the map
+        openForm()
         setLngLatDataOnForm(coordinates)
     }
 }
@@ -82,29 +82,43 @@ function setLngLatDataOnForm(coordinates) {
 }
 
 function saveDataFromForm() {
-    var latValue = document.getElementById("lat").value
-    var lngValue = document.getElementById("lng").value
-    var nameValue = document.getElementById("map-content-form-name").value
-    var emailValue = document.getElementById("map-content-form-email").value
-    var phoneValue = document.getElementById("map-content-form-phone").value
-    var descriptionValue = document.getElementById("map-content-form-description").value
+    var latValue = document.getElementById("lat").value.trim()
+    var lngValue = document.getElementById("lng").value.trim()
+    var nameValue = document.getElementById("map-content-form-name").value.trim()
+    var emailValue = document.getElementById("map-content-form-email").value.trim()
+    var phoneValue = document.getElementById("map-content-form-phone").value.trim()
+    var descriptionValue = document.getElementById("map-content-form-description").value.trim()
 
-    var occurrence = {
-        creationDate: getDateNow(),
-        personalInfo: {
-            name: nameValue,
-            email: emailValue,
-            phone: phoneValue
-        },
-        occurrenceInfo: {
-            longitude: lngValue,
-            latitude: latValue,
-            description: descriptionValue
+    // var addressValue = document.getElementById("map-content-form-address").value.trim()
+    var addressValue = "Rua bla bla 12 - centro" // mocked since we dont have an API to search the address by lng/lat
+
+    if (
+        nameValue.length === 0 ||
+        emailValue.length === 0 ||
+        phoneValue.length  === 0 ||
+        descriptionValue.length === 0
+    ) {
+        alert("Você precisa preencher todos os campos!")
+    } else {
+        var occurrence = {
+            creationDate: getDateNow(),
+            personalInfo: {
+                name: nameValue,
+                email: emailValue,
+                phone: phoneValue
+            },
+            occurrenceInfo: {
+                longitude: lngValue,
+                latitude: latValue,
+                description: descriptionValue,
+                address: addressValue
+            }
         }
-    }
+        console.log(occurrence)
 
-    console.log(occurrence)
-    clearForm()
+        lastUncreatedMarkerId = -1 // since the marker was created, setting id to -1 (default)
+        clearForm()
+    }
 }
 
 function clearForm() {
@@ -116,7 +130,11 @@ function clearForm() {
     document.getElementById("map-content-form-date").value = ""
     document.getElementById("map-content-form-description").value = ""
 
-    openMapcontent() // in this case it will close this content
+    openForm() // in this case it will close this content
+    
+    if (lastUncreatedMarkerId !== -1) {
+        removeMarkerById(lastUncreatedMarkerId)
+    }
 }
 
 function removeMarkerById(id) {
@@ -124,13 +142,19 @@ function removeMarkerById(id) {
     var marker = allMarkers[id];
     marker.remove()
     delete allMarkers[id]
+    lastUncreatedMarkerId = -1
 }
 
 function getDateNow() {
-    var date = new Date()
-    var day = date.getDay()
-    var month = date.getMonth()
-    var year = date.getFullYear()
+    // var date = new Date()
+    // var day = date.getDay()
+    // var month = date.getMonth()
+    // var year = date.getFullYear()
 
-    return `${day}/${month}/${year}`
+    // return `${day}/${month}/${year}`
+    var dt = new Date();
+    var day = dt.getUTCDate()
+    var month = dt.getUTCMonth()+1
+    var year = dt.getUTCFullYear()
+    return (day + '/' + month) + '/' + year
 }
